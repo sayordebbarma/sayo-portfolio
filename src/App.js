18c6598769d1601b543013f14aa98dba2aa6e382
sayordebbarma <sayor.debbarma2021@vitbhopal.ac.in>
2023-03-01 20:54:10 +0530

    added about component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Header from './Components/Header/Header';
 import Home from './Components/Home/Home';
+import About from './Components/About/About';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
 
     <main className="main">
       <Home />
+      <About />
     </main>
     </>
 
---
+++ src/Components/About/Info.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const Info = () => {
+  return (
+    <div className="about_info grid">
+        <div className="about_box">
+            <h3 className="about_title">Experience</h3>
+            <span className="about_subtitle">1 Years</span>
+        </div>
+
+        <div className="about_box">
+            <h3 className="about_title"></h3>
+            <span className="about_subtitle"></span>
+        </div>
+
+        <div className="about_box">
+            <h3 className="about_title"></h3>
+            <span className="about_subtitle"></span>
+        </div>
+    </div>
+  )
+}
+
+export default Info
